f87b519f21cd5a3d94d73877af26af9320ac3b73
Haydn Appleby <haydnba@gmail.com>
2018-10-24 13:07:06 +0100

    refactor functions.php, start add fonts and bootstrap...

--- exercise-16/GameFunctions.php
+++ exercise-16/GameFunctions.php
@@ -1,32 +1,25 @@
 <?php
 
 function convert($input) {
-  if ($input === 0 || $input === 'r') return 'rock';
-  if ($input === 1 || $input === 'p') return 'paper';
-  if ($input === 2 || $input === 's') return 'scissors';
+  if ($input == 0) return 'rock';
+  if ($input == 1) return 'paper';
+  if ($input == 2) return 'scissors';
 }
 
-function result($comp, $human) {
-  if ($comp == $human) return 'draw';
-  if ($comp == 'rock' && $human == 'paper') return 'Human';
-  if ($comp == 'rock' && $human == 'scissors') return 'Computer';
-  if ($comp == 'paper' && $human == 'rock') return 'Computer';
-  if ($comp == 'paper' && $human == 'scissors') return 'Human';
-  if ($comp == 'scissors' && $human == 'rock') return 'Human';
-  if ($comp == 'scissors' && $human == 'paper') return 'Computer';
-}
+function getResult($comp, $human) {
+  if ($comp == $human) return [$comp, $human, 'draw'];
+  if ($comp == 'rock' && $human == 'paper') return [$comp, $human, 'Human'];
+  if ($comp == 'rock' && $human == 'scissors') return [$comp, $human, 'Computer'];
+  if ($comp == 'paper' && $human == 'rock') return [$comp, $human, 'Computer'];
+  if ($comp == 'paper' && $human == 'scissors') return [$comp, $human, 'Human'];
+  if ($comp == 'scissors' && $human == 'rock') return [$comp, $human, 'Human'];
+  if ($comp == 'scissors' && $human == 'paper') return [$comp, $human, 'Computer'];
+} 
 
-$userChoice = $_REQUEST['choice'];
+$result = getResult(convert(rand(0, 2)), convert($_REQUEST['choice']));
 
-$userPlay = convert($userChoice);
-$compPlay = convert(rand(0, 2));
-
-$result = result($compPlay, $userPlay);
-
-// echo "Source: $userChoice; Human: $userPlay; Comp: $compPlay; Result: $result";
-
-if ($result == 'draw') {
-  echo "Human played $userPlay and Computer played $compPlay: Game was a $result!";
+if ($result[2] == 'draw') {
+  echo "Human played $result[1] and Computer played $result[0]: Game was a $result[2]!";
 } else {
-  echo "Human played $userPlay and Computer played $compPlay: $result Wins!";
+  echo "Human played $result[1] and Computer played $result[0]: $result[2] Wins!";
 }
